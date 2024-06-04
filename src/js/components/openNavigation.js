f025f6340b7a5e5af9af4fc2b09f85563a4c78a8
Mateusz Stobiecki <m100biecki@gmail.com>
2024-06-04 19:49:43 +0200

    added closing navigation when link is clicked

--- src/js/components/openNavigation.js
+++ src/js/components/openNavigation.js
@@ -1,12 +1,16 @@
 const openNavigation = () => {
   const navigationWrapper = document.querySelector(".navigagtion__wrapper--js");
   const navigationButton = document.querySelector(".navigation__button--js");
+  const navigationLinks = document.querySelectorAll(".navigation__link");
 
   const handleNavigation = () => {
     navigationWrapper.classList.toggle("navigation__open--js");
   };
 
   navigationButton.addEventListener("click", handleNavigation);
+  navigationLinks.forEach((link) =>
+    link.addEventListener("click", handleNavigation)
+  );
 };
 
 export default openNavigation;
